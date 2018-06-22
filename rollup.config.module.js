5772ac648bcb62a3023e58a8774836702c8b6d6b
vasco <vastur@gmail.com>
2018-06-21 20:39:11 -0700

    Export default module

--- rollup.config.module.js
+++ rollup.config.module.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import { name, dependencies } from './package.json';
 
 export default {
-    input: 'src/index.js',
+    input: 'src/surface.js',
     output: [
         {
             format: 'cjs',
